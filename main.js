90cd34fb31a74cfba2d4c6ed5fa5e9505f3e202f
GabrielGos <131292243+GabrielGos@users.noreply.github.com>
2024-03-08 14:29:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-//https://teachablemachine.withgoogle.com/models/FLE5jjzqh/// //&#128076; = OK// //&#128077; = SIM// //&#128078; = NÃO//
-
 Webcam.set({
     width:350,
     heigth:300,
     imageFormat : 'png' ,
-    pngQuality:1090
+    pngQuality:180
 });
 
 camera = document.getElementById("camera");
@@ -19,9 +17,9 @@ function takeSnapshot()
     });
 }
 
-console.log('ml5 version:', ml5,version);
+console.log('ml5 version:', ml5.version);
 
-classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/FLE5jjzqh/model.json',modelLoaded);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JkZlCYdQS/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded");
@@ -51,38 +49,38 @@ function gotResult(error, results) {
 prediction1 = result[0].label;
 prediction2 = result[1].label;
 speak();
-if (results[0].label == "Ok")
+if (results[0].label == "feliz")
 {
-    document.getElementById("updateEmoji").innerHTML = "&#128076;";
+    document.getElementById("updateEmoji").innerHTML = "&#128522;";
 }
 
 
-if (results[0].label == "Não")
+if (results[0].label == "triste")
 {
-    document.getElementById("updateEmoji").innerHTML = "&#128078;";
+    document.getElementById("updateEmoji").innerHTML = "&#128532;";
 }
 
-if (results[0].label == "Sim")
+if (results[0].label == "bravo")
 {
-    document.getElementById("updateEmoji").innerHTML = "&#128077";
-}
-
-
-
-if (results[0].label == "Ok")
-{
-    document.getElementById("updateEmoji2").innerHTML = "&#128076;";
+    document.getElementById("updateEmoji").innerHTML = "&#128548;";
 }
 
 
-if (results[0].label == "Não")
+
+if (results[1].label == "feliz")
 {
-    document.getElementById("updateEmoji2").innerHTML = "&#128078;";
+    document.getElementById("updateEmoji2").innerHTML = "&#128522;";
 }
 
-if (results[0].label == "Sim")
+
+if (results[1].label == "triste")
 {
-    document.getElementById("updateEmoji2").innerHTML = "&#128077";
+    document.getElementById("updateEmoji2").innerHTML = "&#128532;";
+}
+
+if (results[1].label == "bravo")
+{
+    document.getElementById("updateEmoji2").innerHTML = "&#128548;";
 }
 
 }}
